e7bf0186d95f1ae0f2f37f02e026c0eeb6a2f736
LindsayLew <lmschwehr@gmail.com>
2022-07-03 22:52:18 -0700

    adding my js file so I can add the current date at top

---
+++ script.js
@@ -0,0 +1,8 @@
+//need a moment js bit
+//to show the current date AND/OR time: moment().format('MMMM Do YYYY, h:mm:ss a');
+
+var pageClock = document.querySelector("#currentDay");
+
+function 
+
+    
