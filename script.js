8f72bd3227c300aea1c7fcf2888d0a6bb76a952b
LindsayLew <lmschwehr@gmail.com>
2022-07-05 16:21:50 -0700

    added comments to reflect on later

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 //need a moment js bit
 //to show the current date AND/OR time: moment().format('MMMM Do YYYY, h:mm:ss a');
 
-// why am I bad at this moment thing, my moment keeps coming back undefined.
-
-
-//  const current = moment(today).format("dddd, MMMM, Do YYYY, h:mm:ss a");
-//  $("#currentDay").text(current);
-// console.log(today)
-
-// var today = document.getElementById("#currentDay");
-// moment().format("dddd, MMMM Do YYYY");
-//     console.log()
-
-
 // WOOOOO. IT WORKS. 
-// note to self, always check to make sure your jquery is linked.... will style later.
+// note to self, always check to make sure your jquery is linked.... will style later. probably going to need the actual time as well so the color coded stuff works?
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
