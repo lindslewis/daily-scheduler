eaed5db57324a82c294a2f5801afda8186ef333d
LindsayLew <lmschwehr@gmail.com>
2022-07-05 15:56:03 -0700

    adding button for saving to local storage

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 //need a moment js bit
 //to show the current date AND/OR time: moment().format('MMMM Do YYYY, h:mm:ss a');
 
-var pageClock = document.querySelector("#currentDay");
+// why am I bad at this moment thing, my moment keeps coming back undefined.
 
-function 
+
+//  const current = moment(today).format("dddd, MMMM, Do YYYY, h:mm:ss a");
+//  $("#currentDay").text(current);
+// console.log(today)
+
+var today = moment();
+("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+
+
+
+
+
 
     
