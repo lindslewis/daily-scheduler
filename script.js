38619b002fc70e6a78d528b74ec18c68d38fc2df
LindsayLew <lmschwehr@gmail.com>
2022-07-05 16:58:09 -0700

    adding input spot for events

--- script.js
+++ script.js
@@ -6,6 +6,12 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
+// might need event.result at some point for returning the value of an event (so the button added in the third column)
+//or an event.handler (see form submittals are listed as an example of event.handlers)
+
+var userInput = document.getElementById("eventInput");
+    
+
 
 
 
